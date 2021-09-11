0deafd2f813a9eae3297774848b2ac35e43d70ee
EriWinckler <efwinckler@gmail.com>
2021-09-11 02:04:00 -0400

    first commit

---
+++ src/com/company/cards/Card.java
@@ -0,0 +1,13 @@
+package com.company.cards;
+
+
+public abstract class Card {
+
+    public static final String[] RANKS = {
+            "Ace", "2", "3", "4", "5", "6", "7",
+            "8", "9", "10", "Jack", "Queen", "King"};
+
+    public static final String[] SUITS = {
+            "Clubs", "Diamonds", "Hearts", "Spades"
+    };
+}
